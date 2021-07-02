0922c3f4182f5615232e58c27f860fa2837a0ba6
so7iw <79948954+so7iw@users.noreply.github.com>
2021-07-03 01:12:57 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-    $(document).ready( () =>{
-    $(".add").hide();
-    $(".showHide").click(function(e){
-        e.preventDefault();
-        if ($(this).text() === "Read More") $(this).text("Hide");
-        else $(this).text("Read more");
-        $(this).closest('.caption').children('.add').slideToggle(650);    
-    })
-}); 
