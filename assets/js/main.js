3f964f12f1aec4304545376f16356713aff437d3
Milos-Acer <miki100janovski@gmail.com>
2022-03-10 01:12:57 +0100

    Added cart.

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,14 +34,18 @@ window.onload = () => {
 
         let output = "";
         output += `
-<a class="navbar-brand text-danger" href="#">MaxFit</a>
+<a class="navbar-brand text-danger" href="index.html">MaxFit</a>
+<a class="nav-link cart position-relative d-inline-flex" href="#"><i class="fas fa-shopping-cart" id="cart"></i><span class="cart-basket d-flex align-items-center justify-content-center bg-danger">
+3
+</span></a>
+
 		  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		  </button>
 		  <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ">
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
                 </li>
             `
         data.forEach(element => {
